35115a2af5abf00fc0160f22ceed987d5a49af52
Fangzhou Li <fzli0805@gmail.com>
2022-11-03 22:28:10 -0700

    added all the scripts

---
+++ scripts/2_run_uncertain.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=fa_uncertain
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=fzli@ucdavis.edu
+#SBATCH --output=/home/lfz/git/FoodAtlas/logs/%j.out
+#SBATCH --error=/home/lfz/git/FoodAtlas/logs/%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32
+#SBATCH --time=10-00:00:00
+
+for run in {1..100}
+do
+    for round in {1..10}
+    do
+        ./run_round.sh uncertain $run $round
+    done
+done
